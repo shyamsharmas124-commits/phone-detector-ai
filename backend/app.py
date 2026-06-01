d2c931e9123bc95b55abefa9da8d044732539bd5
shyamsharmas124commits <2006sharma.shyam@gmail.com>
2026-06-01 12:33:48 +0530

    changed the app.py code a little bit

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,6 @@ from detector import detect_phone
 import cv2
 import numpy as np
 import base64
-import os
 import traceback
 
 app = Flask(__name__)
@@ -23,29 +22,19 @@ def home():
         "message": "Phone Detector API"
     })
 
-
 @app.route("/detect", methods=["POST"])
 def detect():
 
     try:
 
-        print("Request received")
-
         data = request.json
 
-        if not data:
-            return jsonify({
-                "error": "No JSON data"
-            }), 400
-
-        if "image" not in data:
+        if not data or "image" not in data:
             return jsonify({
                 "error": "No image provided"
             }), 400
 
         image_data = data["image"]
-
-        print("Image received")
 
         encoded_data = image_data.split(",")[1]
 
@@ -59,36 +48,15 @@ def detect():
             cv2.IMREAD_COLOR
         )
 
-        if img is None:
-            return jsonify({
-                "error": "Image decode failed"
-            }), 400
-
-        print("Running AI detection")
-
         result = detect_phone(img)
-
-        print("Detection completed")
 
         return jsonify(result)
 
     except Exception as e:
 
-        print("========== ERROR ==========")
-        print(str(e))
+        print("ERROR:", str(e))
         traceback.print_exc()
-        print("===========================")
 
         return jsonify({
             "error": str(e)
         }), 500
-
-
-if __name__ == "__main__":
-
-    PORT = int(os.environ.get("PORT", 5000))
-
-    app.run(
-        host="0.0.0.0",
-        port=PORT
-    )
